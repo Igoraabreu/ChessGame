3e1805bc0d710e6df906e9c0f142ea2cd569775a
Igor Abreu <igorbrasil49@hotmail.com>
2019-04-27 18:44:46 -0300

    Criando a classe PosicaoXadrez

---
+++ xadrez_console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez 
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+            // o 'a' char tem valor de 97 na tabela ASCII e assim é possivel fazer a operação Coluna - 'a' para definir a coluna na matriz
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha; 
+        }
+    }
+}
